2de3c8451d35896d29283d5aea47b674fffc8da9
Janzell <94756559+Darkify19@users.noreply.github.com>
2024-12-02 21:05:04 +0800

    connect to front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'  # Your frontend URL
+    origins 'https://samyang-front-g7jeey08v-darkifys-projects.vercel.app/'  # Your frontend URL
     resource '*',
              headers: :any,
              methods: [:get, :post, :patch, :put, :delete, :options, :head],
