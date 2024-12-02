395575a509914c70f919da59737a55f910edc6ba
Janzell <94756559+Darkify19@users.noreply.github.com>
2024-12-02 23:01:23 +0800

    oh updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://samyang-front-g7jeey08v-darkifys-projects.vercel.app/'  # Your frontend URL
+    origins 'https://samyang-front-g7jeey08v-darkifys-projects.vercel.app'  # No trailing slash
     resource '*',
              headers: :any,
              methods: [:get, :post, :patch, :put, :delete, :options, :head],
