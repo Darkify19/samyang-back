6b730d92e1b128e6df24931d2311a160ba55c011
Janzell <94756559+Darkify19@users.noreply.github.com>
2024-12-03 11:23:31 +0800

    flexible cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
-# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://samyang-front.vercel.app/'  # Your frontend URL
+    # Allow frontend in development (localhost) and production (vercel) to access the backend
+    origins 'http://localhost:8080', 'https://samyang-front.vercel.app'  # Your frontend URL(s)
+
     resource '*',
              headers: :any,
              methods: [:get, :post, :patch, :put, :delete, :options, :head],
